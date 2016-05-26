54e2b285caf7c3d923a58d2b10808246f5e98a7e
lanouhn <1763168119@qq.com>
2016-05-26 17:31:40 +0800

    跟新

---
+++ Test2/Test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by lanouhn on 16/5/26.
+//  Copyright © 2016年 book. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test2/Test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test2
+//
+//  Created by lanouhn on 16/5/26.
+//  Copyright © 2016年 book. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
